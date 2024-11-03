b50a5f65e9f93220bd9ae2e4633eff88d7594d9e
Aashish Kumar Jha <ash1999.official@gmail.com>
2024-11-03 12:08:41 +0530

    Initial commit

---
+++ RestWeek/src/main/java/com/HW/WEEK2/RestWeek/RestWeekApplication.java
@@ -0,0 +1,13 @@
+package com.HW.WEEK2.RestWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWeekApplication.class, args);
+	}
+
+}
---
+++ RestWeek/src/test/java/com/HW/WEEK2/RestWeek/RestWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HW.WEEK2.RestWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
